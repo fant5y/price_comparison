35aac0b19551ce68d2c2e2d11d2a399957c65a7f
Vroni <103374464+fant5y@users.noreply.github.com>
2023-12-16 22:54:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,40 +48,7 @@ def append_to_csv(dataframe, csv_file_path, sep=","):
         dataframe.to_csv(csv_file_path, index=False, sep=sep)
 
 
-st.set_page_config(page_title="Some Calculations", layout="wide")
-st.title("Calculations")
-col0, col1, col2, col3, col4 = st.columns(
-    [2, 1, 1, 1, 1],
-    gap="small",
-)
-
-material_df = load_material_data(CSV_PATH, EMPTY_MATERIAL_DATA)
-if "data" not in st.session_state:
-    st.session_state.material_df = material_df.to_dict()
-with col0:
-    st.text_input(label="Product Name or Number", key="product_identifier")
-    st.text_input(label="URL", key="link")
-with col1:
-    st.number_input(label="Length", step=10, key="material_length", help="in mm")
-    st.number_input(label="Width", step=10, key="material_width", help="in mm")
-    st.number_input(label="Height", step=10, key="material_height", help="in mm")
-with col2:
-    st.number_input(label="Amount", step=1, key="material_amount")
-    st.number_input(label="Price", step=1.0, key="material_price", help="in €")
-with col3:
-    pass
-
-if st.button("Calculate"):
-    input_values = [
-        st.session_state.get("material_width", None),
-        st.session_state.get("material_length", None),
-        st.session_state.get("material_height", None),
-        st.session_state.get("material_amount", None),
-        st.session_state.get("material_price", None),
-        st.session_state.get("link", None),
-        st.session_state.get("product_identifier", None),
-    ]
-
+def calculate_prices(material_list):
     width_in_meters = st.session_state.get("material_width") / 1000
     length_in_meters = st.session_state.get("material_length") / 1000
     single_amount_square_meters = width_in_meters * length_in_meters
@@ -98,24 +65,51 @@ if st.button("Calculate"):
     )
     price_per_qm = st.session_state.material_price / qm if qm != 0 and not None else 0
 
-    input_values.append(qm)
-    input_values.append(price_per_unit)
-    input_values.append(price_per_qm)
+    material_list.append(qm)
+    material_list.append(price_per_unit)
+    material_list.append(price_per_qm)
 
-    with col3:
-        pass
-        # st.write(width_in_meters)
-        # st.write(length_in_meters)
-        # st.write(single_amount_square_meters)
-        # st.write(st.session_state.material_amount)
-        # st.write(
-        #     f"{width_in_meters} * {length_in_meters} = {single_amount_square_meters}\n{single_amount_square_meters} * {st.session_state.material_amount} = {st.session_state.material_amount * single_amount_square_meters}"
-        # )
-
-    with col4:
+    with st.sidebar():
         st.write(f"{qm:.2f} qm")
         st.write(f"{price_per_qm:.2f} € per qm")
         st.write(f"{price_per_unit:.2f} € per unit")
+
+    return material_list
+
+
+st.set_page_config(page_title="Some Calculations", layout="wide")
+st.title("Calculations")
+col0, col1, col2, col3, col4 = st.columns(
+    [2, 1, 1, 1, 1],
+    gap="small",
+)
+
+material_df = load_material_data(CSV_PATH, EMPTY_MATERIAL_DATA)
+
+if "data" not in st.session_state:
+    st.session_state.material_df = material_df.to_dict()
+
+with st.sidebar():
+    st.text_input(label="Product Name or Number", key="product_identifier")
+    st.text_input(label="URL", key="link")
+    st.number_input(label="Length", step=10, key="material_length", help="in mm")
+    st.number_input(label="Width", step=10, key="material_width", help="in mm")
+    st.number_input(label="Height", step=10, key="material_height", help="in mm")
+    st.number_input(label="Amount", step=1, key="material_amount")
+    st.number_input(label="Price", step=1.0, key="material_price", help="in €")
+
+if st.button("Calculate"):
+    input_values = [
+        st.session_state.get("material_width", None),
+        st.session_state.get("material_length", None),
+        st.session_state.get("material_height", None),
+        st.session_state.get("material_amount", None),
+        st.session_state.get("material_price", None),
+        st.session_state.get("link", None),
+        st.session_state.get("product_identifier", None),
+    ]
+
+    input_values = calculate_prices(input_values)
 
     for key, value in zip(EMPTY_MATERIAL_DATA.keys(), input_values):
         EMPTY_MATERIAL_DATA[key].append(value)
@@ -144,7 +138,6 @@ st.download_button(
     data=csv_string,
     file_name="Plywood Price Comparison.csv",
 )
-
 
 st.bar_chart(material_data, x="product_identifier", y="price_per_qm")
 st.dataframe(
